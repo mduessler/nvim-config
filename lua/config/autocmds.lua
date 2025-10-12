19a17f88e3788ea83a1deb7531e85caab720159a
Max Duessler <max.duessler@posteo.net>
2025-10-12 06:55:48 +0200

    Updated package to download for registry.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,11 +5,27 @@ vim.api.nvim_create_autocmd("User", {
 			print("Do nothing to install mason servers?")
 			local require_safe = require("utils.require_safe")
 			local servers = require_safe("lsp.servers")
-			if not servers then
+			local registry = require_safe("mason-registry")
+			if not (servers and registry) then
 				return
 			end
 			for _, server in ipairs(servers) do
 				vim.cmd("LspInstall " .. server)
+				if not registry.has_package(server) then
+					print("Don't know package " .. server)
+				else
+					local pkg = registry.get_package(server)
+					if not pkg:is_installed() then
+						print("[Headless] Installing " .. server .. " ...")
+						pkg:install()
+						vim.wait(120000, function()
+							return pkg:is_installed()
+						end, 500)
+						print("[Headless] Installed: " .. server)
+					else
+						print("[Headless] Already installed: " .. server)
+					end
+				end
 				print("Installed Language server: " .. server)
 			end
 			vim.cmd("MasonToolsInstallSync")
