935606e5ccff0b4dee3c02a617c8e45b1816ce23
Max Duessler <max.duessler@posteo.net>
2025-10-12 11:01:14 +0200

    Made table to old style.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -15,8 +15,18 @@ return {
 		local signs = require_safe("config.signs")
 		local tables = require_safe("utils.tables")
 
-		if not (ls and mason and mason_lspconfig and mason_tool_installer and signs and tables) then
+		if
+			not (formater and linter and ls and mason and mason_lspconfig and mason_tool_installer and signs and tables)
+		then
 			return
+		end
+
+		local linter_formater = {}
+		for _, value in pairs(formater) do
+			linter_formater[#linter_formater + 1] = value
+		end
+		for _, value in pairs(linter) do
+			linter_formater[#linter_formater + 1] = value
 		end
 
 		local lsp = {}
@@ -35,7 +45,7 @@ return {
 		mason_lspconfig.setup({ ensure_installed = lsp, automatic_installation = true, automatic_enable = false })
 
 		mason_tool_installer.setup({
-			ensure_installed = tables.merge_array(linter, formater),
+			ensure_installed = linter_formater,
 			automatic_installation = true,
 		})
 	end,
