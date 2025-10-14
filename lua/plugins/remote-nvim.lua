c0e6f62f8074a10dc2f559c9c228617f5a185481
Max Düssler <max.duessler@posteo.net>
2025-10-14 11:49:20 +0200

    Added keys.

--- lua/plugins/remote-nvim.lua
+++ lua/plugins/remote-nvim.lua
@@ -6,6 +6,16 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
+	keys = {
+		{ "<leader>s", "<NOP>", desc = "SSH" },
+		{ "<leader>sg", "<cmd>RemoteStart<CR>", desc = "Start ssh" },
+		{ "<leader>sq", "<cmd>RemoteStop<CR>", desc = "Quit ssh" },
+		{ "<leader>si", "<cmd>RemoteInfo<CR>", desc = "Ssh info" },
+		{ "<leader>sc", "<cmd>RemoteCleanup<CR>", desc = "Cleanup workspace and config of remote nvim" },
+		{ "<leader>sd", "<cmd>RemoteConfigDel<CR>", desc = "Delete record or remote instance" },
+		{ "<leader>sl", "<cmd>RemoteLog<CR>", desc = "Open ssh logs." },
+	},
+
 	config = function()
 		local require_safe = require("utils.require_safe")
 
