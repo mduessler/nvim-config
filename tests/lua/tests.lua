0546b393d561b3502cfb8b1de8e46d0713b03acd
Max Düssler <max.duessler@posteo.net>
2025-11-23 05:06:29 +0100

    Mocking vim variable.

--- tests/lua/tests.lua
+++ tests/lua/tests.lua
@@ -1,3 +1,15 @@
 local lu = require("luaunit")
 
+vim = {
+	notify = function(_, _) end,
+	log = {
+		levels = {
+			WARN = 1,
+			ERROR = 2,
+		},
+	},
+}
+
+require("tests.lua.utils.request_tests")
+
 os.exit(lu.LuaUnit.run())
