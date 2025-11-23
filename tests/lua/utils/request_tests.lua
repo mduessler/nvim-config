0172b7ccfaccfdfa9ac57d43807861144817d1bb
Max Düssler <max.duessler@posteo.net>
2025-11-23 03:54:54 +0100

    Moved mock.

--- tests/lua/utils/request_tests.lua
+++ tests/lua/utils/request_tests.lua
@@ -2,42 +2,47 @@ local luaunit = require("luaunit")
 
 local TestRequest = {}
 
+local url_success = "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS"
+local url_failure = "https://api.github.com/repos/mduessler/nvim-config/commits/FAIL"
+
+package.loaded["utils.require_safe"] = function(name)
+	if name == "socket.http" then
+		return {
+			request = function(url)
+				if url == "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS" then
+					return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 200
+				else
+					return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 500
+				end
+			end,
+		}
+	end
+	if name == "cjson" then
+		return {
+			decode = function(_)
+				return { ok = true }
+			end,
+		}
+	end
+end
+
 local M = require("utils.request")
 
 TestRequest.test_get = function()
-	package.loaded["utils.require_safe"] = function(name)
-		if name == "socket.http" then
-			return {
-				request = function(url)
-					if url == "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS" then
-						return '{"committer":{"data":"2025-11-22T01:22:03Z"}}', 200
-					else
-						return '{"committer":{"data":"2025-11-22T01:22:03Z"}}', 500
-					end
-				end,
-			}
-		end
-		if name == "cjson" then
-			return {
-				decode = function(_)
-					return { ok = true }
-				end,
-			}
-		end
-	end
 	local function test_success()
-		local data = M.get("https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS")
+		local data = M.get(url_success)
 		luaunit.assertEqual(data, { ok = true })
 	end
 	local function test_failure()
-		local url = "https://api.github.com/repos/mduessler/nvim-config/commits/FAIL"
-		local data = M.get(url)
+		local data = M.get(url_failure)
 		luaunit.assertNil(data)
-		luaunit.assertErrorMsgEquals("Request to " .. url .. " failed with 500")
+		luaunit.assertErrorMsgEquals("Request to " .. url_failure .. " failed with 500")
 	end
 
 	test_success()
 	test_failure()
 end
+
+TestRequest.test_get_json = function() end
 
 return TestRequest
