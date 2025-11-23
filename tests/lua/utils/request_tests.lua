937a8d50b95a21d22b1c77b6ad1f95113c5c93a2
Max Düssler <max.duessler@posteo.net>
2025-11-23 04:22:55 +0100

    Changed test structure.

--- tests/lua/utils/request_tests.lua
+++ tests/lua/utils/request_tests.lua
@@ -28,59 +28,52 @@ local request = require("utils.request")
 
 local TestRequest = {}
 
-TestRequest.test_get = function()
-	local function test_success()
-		local response = request.get(url_success)
-		luaunit.assertEqual(response, { ok = true })
-	end
-	local function test_failure()
-		local response = request.get(url_failure)
-		luaunit.assertNil(response)
-		luaunit.assertErrorMsgEquals("Request to " .. url_failure .. " failed with 500")
-	end
-
-	test_success()
-	test_failure()
+function TestRequest:test_getsuccess()
+	local response = request.get(url_success)
+	luaunit.assertEqual(response, { ok = true })
 end
 
-TestRequest.test_get_json = function()
-	local function test_response_is_nil()
-		local data = request.get_json(url_failure)
-		luaunit.assertNil(data)
-	end
-	local function test_decode_failure()
-		package.loaded["utils.require_safe"] = function(name)
-			if name == "socket.http" then
-				return {
-					request = function(url)
-						if url == "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS" then
-							return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 200
-						else
-							return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 500
-						end
-					end,
-				}
-			end
-			if name == "cjson" then
-				return {
-					decode = function()
-						error("bad json")
-					end,
-				}
-			end
+function TestRequest:test_getfailure()
+	local response = request.get(url_failure)
+	luaunit.assertNil(response)
+	luaunit.assertErrorMsgEquals("Request to " .. url_failure .. " failed with 500")
+end
+
+function TestRequest:test_get_json_is_nil()
+	local data = request.get_json(url_failure)
+	luaunit.assertNil(data)
+end
+
+function TestRequest:test_get_json_decode_failure()
+	package.loaded["utils.require_safe"] = function(name)
+		if name == "socket.http" then
+			return {
+				request = function(url)
+					if url == "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS" then
+						return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 200
+					else
+						return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 500
+					end
+				end,
+			}
 		end
-		local request_ = require("utils.request")
-		local data = request_.get_json(url_success)
-		luaunit.assertNil(data)
-		luaunit.assertErrorMsgEquals("Can not decode response of request to " .. url_success .. ".")
+		if name == "cjson" then
+			return {
+				decode = function()
+					error("bad json")
+				end,
+			}
+		end
 	end
-	local function test_decode_success()
-		local data = request.get_json(url_success)
-		luaunit.assertEqual(data, { ok = true })
-	end
-	test_response_is_nil()
-	test_decode_failure()
-	test_decode_success()
+	local request_ = require("utils.request")
+	local data = request_.get_json(url_success)
+	luaunit.assertNil(data)
+	luaunit.assertErrorMsgEquals("Can not decode response of request to " .. url_success .. ".")
+end
+
+function TestRequest:test_get_json_decode_success()
+	local data = request.get_json(url_success)
+	luaunit.assertEqual(data, { ok = true })
 end
 
 return TestRequest
