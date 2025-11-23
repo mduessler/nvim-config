b309f03c1db57528cab042977e581ab6d3e7d163
Max Düssler <max.duessler@posteo.net>
2025-11-23 04:18:32 +0100

    Finished test for get_json.

--- tests/lua/utils/request_tests.lua
+++ tests/lua/utils/request_tests.lua
@@ -43,6 +43,44 @@ TestRequest.test_get = function()
 	test_failure()
 end
 
-TestRequest.test_get_json = function() end
+TestRequest.test_get_json = function()
+	local function test_response_is_nil()
+		local data = request.get_json(url_failure)
+		luaunit.assertNil(data)
+	end
+	local function test_decode_failure()
+		package.loaded["utils.require_safe"] = function(name)
+			if name == "socket.http" then
+				return {
+					request = function(url)
+						if url == "https://api.github.com/repos/mduessler/nvim-config/commits/SUCCESS" then
+							return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 200
+						else
+							return '{"committer":{"data":"2025-11-22t01:22:03z"}}', 500
+						end
+					end,
+				}
+			end
+			if name == "cjson" then
+				return {
+					decode = function()
+						error("bad json")
+					end,
+				}
+			end
+		end
+		local request_ = require("utils.request")
+		local data = request_.get_json(url_success)
+		luaunit.assertNil(data)
+		luaunit.assertErrorMsgEquals("Can not decode response of request to " .. url_success .. ".")
+	end
+	local function test_decode_success()
+		local data = request.get_json(url_success)
+		luaunit.assertEqual(data, { ok = true })
+	end
+	test_response_is_nil()
+	test_decode_failure()
+	test_decode_success()
+end
 
 return TestRequest
