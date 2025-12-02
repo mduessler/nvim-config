5d0df53b46943947faf4b2cd28b87db9517d0876
Max Düssler <max.duessler@posteo.net>
2025-12-02 10:30:29 +0100

    Tell neovim to first use lua5.1 cause of luajit.

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
 vim.cmd("set encoding=UTF-8")
 
+-- TODO: Write pathes dynamic.
+local home = os.getenv("HOME")
+package.path = home
+	.. "/.local/share/luarocks/share/lua/5.1/?.lua;"
+	.. home
+	.. "/.local/share/luarocks/share/lua/5.1/?/init.lua;"
+	.. package.path
+
+package.cpath = home
+	.. "/.local/share/luarocks/lib/lua/5.1/?.so;"
+	.. home
+	.. "/.local/share/luarocks/lib64/lua/5.1/?.so;"
+	.. package.cpath
+
 -- Remove Neovim 0.10 default keymaps you don't want
 local status, utils = pcall(require, "utils.key")
 
@@ -14,7 +28,7 @@ vim.g.mapleader = " "
 
 require("config")
 
-vim.notify = require("notify")
+-- vim.notify = require("notify")
 
 require("core.keymaps")
 require("core")
