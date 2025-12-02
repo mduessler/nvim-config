dc2eed8c4fda794737a3b881d181ed07aabf5fe0
Max Düssler <max.duessler@posteo.net>
2025-12-02 11:16:40 +0100

    Changed request.

--- lua/utils/request.lua
+++ lua/utils/request.lua
@@ -1,40 +1,38 @@
 local require_safe = require("utils.require_safe")
+local http = require_safe("socket.http")
 local lunajson = require_safe("lunajson")
 
-if not lunajson then
+if not (http and lunajson) then
 	return
 end
 
 local M = {}
 
-M.get_new = function(url)
-	local output = vim.fn.system("curl", { url })
-	if vim.v.shell_error ~= 0 then
-		vim.notify("Request to " .. url .. " failed with " .. tostring(vim.v.shell_error) .. ".", vim.log.levels.WARN)
+M.get = function(url)
+	local response = http.request(url)
+
+	if response.status ~= 200 then
+		vim.notify("Request to " .. url .. " failed with " .. tostring(response.status) .. ".", vim.log.levels.WARN)
+		return nil
+	end
+	return response
+end
+
+M.get_json = function(url)
+	local resp_body, _ = M.get(url)
+	if not resp_body then
 		return nil
 	end
 
-	local function parse_header(raw)
-		local headers = {}
-		for line in raw:gmatch("[^\r\n]+") do
-			local key, value = line:match("([^:]+):%s*(.*)")
-			if key and value then
-				headers[key] = value
-			end
-		end
-		for k, v in pairs(headers) do
-			print(k, v)
-		end
-		return headers
+	local data, pos, err = lunajson.decode(resp_body)
+
+	if not data then
+		vim.notify(
+			"Can not decode response of request to " .. url .. ".\nJSON parse error at position " .. pos .. ":" .. err,
+			vim.log.levels.ERROR
+		)
 	end
-
-	local header_raw, body_raw = output:match("([\0-\255]*)\r?\n\r?\n([\0-\255]*)")
-
-	local response = {
-		header = parse_header(header_raw),
-	}
-
-	return response, body_raw
+	return data
 end
 
 return M
