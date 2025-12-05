57451f1efaa120eb50b515d070dfd71d0cae9ea2
Max Düssler <max.duessler@posteo.net>
2025-12-05 10:31:47 +0100

    Added handler to check if update is available.

---
+++ lua/utils/update/handler.lua
@@ -0,0 +1,9 @@
+local handler = vim.fn.stdpath("config") .. "/lua/utils/update/handler.lua"
+
+vim.system({ "lua", handler }, { text = true }, function(result)
+	if result.code == 0 then
+		vim.notify("Config is up to date", vim.log.levels.DEBUG)
+	else
+		vim.notify("New config version is out. Compile it with", vim.log.levels.SUCCESS)
+	end
+end)
