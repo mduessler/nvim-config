e59a11a4c95039cd80ed452503aa88c5e39d840d
Max Düssler <max.duessler@posteo.net>
2026-02-21 03:06:36 +0100

    Moved autocommand to own file.

--- lua/utils/logger.lua
+++ lua/utils/logger.lua
@@ -227,9 +227,4 @@ local M = {
 	end,
 }
 
-vim.api.nvim_create_user_command("OpenLog", function()
-	local file_name = string.format("%s/%s.log", LOCAL.config.path, LOCAL.config.name)
-	vim.cmd("tabnew " .. file_name)
-end, { desc = "Open the nvim log file" })
-
 return M
