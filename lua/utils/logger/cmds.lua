04f82d5339516d31a5a31d308e01afa46017416c
Max Düssler <max.duessler@posteo.net>
2026-02-21 10:40:12 +0100

    Added code to get line n of the logfile.

--- lua/utils/logger/cmds.lua
+++ lua/utils/logger/cmds.lua
@@ -18,3 +18,27 @@ vim.api.nvim_create_user_command("LOGLast", function()
 	print(msg)
 	vim.fn.setreg("+", msg)
 end, { desc = "Copy the last log message to the clipboard and print it." })
+
+vim.api.nvim_create_user_command("LOGLineN", function(opts)
+	local number = tonumber(opts.args)
+	if not number then
+		error("Please provide a valid line number", vim.log.levels.ERROR)
+		return
+	end
+
+	local file_name = string.format("%s/%s.log", LOCAL.config.path, LOCAL.config.name)
+	local file = io.open(file_name, "r")
+	if file then
+		local cur = 0
+		for line in file:lines() do
+			cur = cur + 1
+			if cur == number then
+				local msg = line:match("| [A-Z]+ | (.*)")
+				print(msg)
+				vim.fn.setreg("+", msg)
+				file:close()
+				return
+			end
+		end
+	end
+end, { desc = "Copy the last log message of line N to the clipboard and print it." })
