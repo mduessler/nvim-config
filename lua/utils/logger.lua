4cca027ac979b0dc0eed8ce2b0171de98837a716
Max Düssler <max.duessler@posteo.net>
2026-02-21 02:58:28 +0100

    Added user command to open the log file.

--- lua/utils/logger.lua
+++ lua/utils/logger.lua
@@ -227,4 +227,9 @@ local M = {
 	end,
 }
 
+vim.api.nvim_create_user_command("OpenLog", function()
+	local file_name = string.format("%s/%s.log", LOCAL.config.path, LOCAL.config.name)
+	vim.cmd("tabnew " .. file_name)
+end, { desc = "Open the nvim log file" })
+
 return M
