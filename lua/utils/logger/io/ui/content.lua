3d2769880e2a5b664142cb7e6ec504f38a946716
Max Düssler <max.duessler@posteo.net>
2026-02-23 11:06:57 +0100

    Fixed function to build info components.

--- lua/utils/logger/io/ui/content.lua
+++ lua/utils/logger/io/ui/content.lua
@@ -8,22 +8,31 @@ end
 
 local M = {}
 
-local function info_line(content, level, entry)
-	local strings = {
-		level = LOCAL.signs[level:lower()] .. " " .. level,
-		line = LOCAL.signs.line .. tostring(entry) .. " ",
-		time = LOCAL.signs.time .. " " .. os.date("%H:%M:%S"),
-	}
-	local level_width = vim.fn.strdisplaywidth(strings.level)
-	local line_width = vim.fn.strdisplaywidth(strings.line)
-	local time_width = vim.fn.strdisplaywidth(strings.time)
-	strings.padding = LOCAL.signs.padding:rep(LOCAL.width - (3 + level_width + line_width + time_width))
-	content[1] = strings
-end
-
 M.create = function(level, entry)
 	local content = {}
-	info_line(content, level, entry)
+
+	local function info_line()
+		local level_string = LOCAL.signs[level:lower()] .. " " .. level
+		local level_width = vim.fn.strdisplaywidth(level_string)
+		local line_string = LOCAL.signs.line .. tostring(entry) .. " "
+		local line_width = vim.fn.strdisplaywidth(line_string)
+		local time_string = LOCAL.signs.time .. " " .. os.date("%H:%M:%S")
+		local time_width = vim.fn.strdisplaywidth(time_string)
+		local line_time_pad = 3
+
+		local components = {
+			level_string,
+			LOCAL.signs.padding:rep(LOCAL.width - (2 + level_width + line_width + line_time_pad + time_width)),
+			line_string,
+			LOCAL.padding:rep(line_time_pad),
+			time_string,
+		}
+		content[1] = components
+	end
+
+	info_line()
+
+	return content
 end
 
 return M
