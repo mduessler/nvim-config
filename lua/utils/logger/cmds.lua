ef18aa6fd8fbf2865819210122b831e3dd4afde0
Max Düssler <max.duessler@posteo.net>
2026-02-24 05:02:52 +0100

    Added file number check to single function.

--- lua/utils/logger/cmds.lua
+++ lua/utils/logger/cmds.lua
@@ -6,31 +6,45 @@ if not config then
 	return
 end
 
---- Opens the logfile in a new tab
-vim.api.nvim_create_user_command("LOGOpen", function(opts)
-	local arg = opts.args
+--- Function to validate if the given number is a valid rotate file of the nvim logger.
+---@param arg string The given number as string.
+---@return string|nil name Log file to open
+local function validate_file_name(arg)
 	local name = config.config.name
 	if arg ~= "" then
 		local number = tonumber(arg, 10)
 		if config.config.files < number or number < 0 then
 			error("Argument needs to be between 0 < ARG < " .. tostring(config.config.files), vim.log.levels.ERROR)
-			return
+			return nil
 		end
 		name = config.config.name .. "-" .. arg
 	end
-	local file_name = string.format("%s/%s.log", config.config.path, name)
-	vim.cmd("tabnew " .. file_name)
+	return name
+end
+
+--- Opens the logfile in a new tab
+---@useage: :LOGOpen [N]   (opens nvim-N.log, default opens nvim.log)
+vim.api.nvim_create_user_command("LOGOpen", function(opts)
+	local name = validate_file_name(opts.args)
+	if name then
+		local file_name = string.format("%s/%s.log", config.config.path, name)
+		vim.cmd("tabnew " .. file_name)
+	end
 end, { desc = "Open the nvim log file", nargs = "?" })
 
 --- Uses `tail` to fetch the last line, extracts the message part,
 --- prints it and copies it to the clipboard.
-vim.api.nvim_create_user_command("LOGLast", function()
-	local file_name = string.format("%s/%s.log", config.config.path, config.config.name)
-	local last_line = vim.fn.system("tail -n 1 " .. vim.fn.shellescape(file_name)):gsub("\n$", "")
-	local msg = last_line:match("| [A-Z]+ | (.*)")
-	print(msg)
-	vim.fn.setreg("+", msg)
-end, { desc = "Copy the last log message to the clipboard and print it." })
+---@useage: :LOGLast [N]   (returns last log message from nvim log file.)
+vim.api.nvim_create_user_command("LOGLast", function(opts)
+	local name = validate_file_name(opts.args)
+	if name then
+		local file_name = string.format("%s/%s.log", config.config.path, name)
+		local last_line = vim.fn.system("tail -n 1 " .. vim.fn.shellescape(file_name)):gsub("\n$", "")
+		local msg = last_line:match("| [A-Z]+ | (.*)")
+		print(msg)
+		vim.fn.setreg("+", msg)
+	end
+end, { desc = "Copy the last log message to the clipboard and print it.", nargs = "?" })
 
 --- Reads the log file sequentially until it reaches the requested line,
 --- then prints and copies the message part.
