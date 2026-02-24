23a76310bb86f6a55946cafb254a9bf6ce8e3df6
Max Düssler <max.duessler@posteo.net>
2026-02-24 08:54:05 +0100

    Undoing auto-update.

--- init.lua
+++ init.lua
@@ -1,19 +1,5 @@
 vim.cmd("set encoding=UTF-8")
 
--- TODO: Write pathes dynamic.
-local home = os.getenv("HOME")
-package.path = home
-	.. "/.local/share/luarocks/share/lua/5.1/?.lua;"
-	.. home
-	.. "/.local/share/luarocks/share/lua/5.1/?/init.lua;"
-	.. package.path
-
-package.cpath = home
-	.. "/.local/share/luarocks/lib/lua/5.1/?.so;"
-	.. home
-	.. "/.local/share/luarocks/lib64/lua/5.1/?.so;"
-	.. package.cpath
-
 -- Remove Neovim 0.10 default keymaps you don't want
 local status, utils = pcall(require, "utils.key")
 
