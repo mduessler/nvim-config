a2dc3a3305f76fb49feb15d8fffcaa50321c94cb
Max Düssler <max.duessler@posteo.net>
2026-02-24 05:20:13 +0100

    Write comments for commands.

--- lua/utils/logger/cmds.lua
+++ lua/utils/logger/cmds.lua
@@ -6,13 +6,28 @@ if not config then
 	return
 end
 
+--- Validates if a given input is a valid number.
+---@param arg string Given input of the user command.
+---@return integer|nil number On success returns the number, otherwise nil.
+local function validate_number(arg)
+	local number = tonumber(arg, 10)
+	if not number then
+		error("Please provide a valid line number", vim.log.levels.ERROR)
+		return nil
+	end
+	return number
+end
+
 --- Function to validate if the given number is a valid rotate file of the nvim logger.
 ---@param arg string The given number as string.
 ---@return string|nil name Log file to open
-local function validate_file_name(arg)
+local function validate_log_number(arg)
 	local name = config.config.name
 	if arg ~= "" then
-		local number = tonumber(arg, 10)
+		local number = validate_number(arg)
+		if number == nil then
+			return
+		end
 		if config.config.files < number or number < 0 then
 			error("Argument needs to be between 0 < ARG < " .. tostring(config.config.files), vim.log.levels.ERROR)
 			return nil
@@ -23,9 +38,9 @@ local function validate_file_name(arg)
 end
 
 --- Opens the logfile in a new tab
----@useage: :LOGOpen [N]   (opens nvim-N.log, default opens nvim.log)
+---@usage: :LOGOpen [N]   (Opens nvim-N.log in a new tabpage, default opens nvim.log)
 vim.api.nvim_create_user_command("LOGOpen", function(opts)
-	local name = validate_file_name(opts.args)
+	local name = validate_log_number(opts.args)
 	if name then
 		local file_name = string.format("%s/%s.log", config.config.path, name)
 		vim.cmd("tabnew " .. file_name)
@@ -34,9 +49,9 @@ end, { desc = "Open the nvim log file", nargs = "?" })
 
 --- Uses `tail` to fetch the last line, extracts the message part,
 --- prints it and copies it to the clipboard.
----@useage: :LOGLast [N]   (returns last log message from nvim log file.)
+---@usage: :LOGLast [N]   (Prints last line from file nvim-N.log, default prints nvim.log)
 vim.api.nvim_create_user_command("LOGLast", function(opts)
-	local name = validate_file_name(opts.args)
+	local name = validate_log_number(opts.args)
 	if name then
 		local file_name = string.format("%s/%s.log", config.config.path, name)
 		local last_line = vim.fn.system("tail -n 1 " .. vim.fn.shellescape(file_name)):gsub("\n$", "")
@@ -48,20 +63,27 @@ end, { desc = "Copy the last log message to the clipboard and print it.", nargs 
 
 --- Reads the log file sequentially until it reaches the requested line,
 --- then prints and copies the message part.
+---@usage: LOGLineN N [M] (Prints line N from file nvim-M.log, default prints N from file nvim.log)
 vim.api.nvim_create_user_command("LOGLineN", function(opts)
-	local number = tonumber(opts.args)
-	if not number then
-		error("Please provide a valid line number", vim.log.levels.ERROR)
+	local args = opts.args
+	if #args < 1 then
+		error("At least one argument is required", vim.log.levels.ERROR)
 		return
 	end
 
-	local file_name = string.format("%s/%s.log", config.config.path, config.config.name)
+	local line_number = validate_number(args[1])
+	local name = validate_log_number(args[2])
+	if line_number == nil or name == nil then
+		return
+	end
+
+	local file_name = string.format("%s/%s.log", config.config.path, name)
 	local file = io.open(file_name, "r")
 	if file then
 		local cur = 0
 		for line in file:lines() do
 			cur = cur + 1
-			if cur == number then
+			if cur == line_number then
 				local msg = line:match("| [A-Z]+ | (.*)")
 				print(msg)
 				vim.fn.setreg("+", msg)
@@ -70,4 +92,4 @@ vim.api.nvim_create_user_command("LOGLineN", function(opts)
 			end
 		end
 	end
-end, { desc = "Copy the last log message of line N to the clipboard and print it.", nargs = 1 })
+end, { desc = "Copy the last log message of line N to the clipboard and print it.", nargs = "*" })
