0eaf21bc25feadfcb147cb959f4b5990cac0fa0b
Max Düssler <max.duessler@posteo.net>
2026-02-24 08:14:39 +0100

    Refactored code.

--- lua/utils/request.lua
+++ lua/utils/request.lua
@@ -1,44 +1,42 @@
-local http = require("socket.http")
-local lunajson = require("lunajson")
+local require_safe = require("utils.require_safe")
 
-if not (http and lunajson) then
+local http = require_safe("socket.http")
+local lunajson = require_safe("lunajson")
+local logger = require_safe("utils.logger")
+
+if not (http and lunajson and logger) then
 	return
 end
 
 local M = {}
 
+--- Function to get the given url
+---@param url string The url to request
+---@return string|nil body On status code 200 returns the body string, otherwise nil
 M.get = function(url)
 	local body, status = http.request(url)
-
-	if status ~= 200 then
-		if vim then
-			vim.notify("Request to " .. url .. " failed with " .. tostring(status) .. ".", vim.log.levels.WARN)
-		end
+	if type(status) ~= "number" or status ~= 200 then
+		logger.WARN(string.format("Request to %s failed: %s", url, tostring(status)))
 		return nil
 	end
+	logger.DEBUG(string.format("Success: Request to %s.", url))
 	return body
 end
 
+--- Function to parse body of get request
+---@param url string Url to request
+---@return table|nil data On success returns the body as table, otherwise nil.
 M.get_json = function(url)
 	local body = M.get(url)
-	if not body then
+	if body == nil then
 		return nil
 	end
 
 	local data, pos, err = lunajson.decode(body)
 
 	if not data then
-		if vim then
-			vim.notify(
-				"Can not decode response of request to "
-					.. url
-					.. ".\nJSON parse error at position "
-					.. pos
-					.. ":"
-					.. err,
-				vim.log.levels.ERROR
-			)
-		end
+		logger.ERROR(string.format("JSON parse error for %s at position %i: %s", url, pos, err))
+		return nil
 	end
 	return data
 end
