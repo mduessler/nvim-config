37e81ce7de0f85bc8a72b0872417548d6270b871
Max Düssler <max.duessler@posteo.net>
2026-02-27 00:01:34 +0100

    Created main structure for handle.

--- lua/core/update/handler.lua
+++ lua/core/update/handler.lua
@@ -2,9 +2,11 @@ local require_safe = require("utils.require_safe")
 local close_process = require_safe("core.utils.async.close_process")
 local close_timer = require_safe("core.utils.async.close_timer")
 local commit = require_safe("core.update.git.commit")
-local ref_type = require_safe("core.update.git.reference.type")
+local logger = require_safe("utils.logger")
+local ref_name = require_safe("core.update.git.reference.type")
+local ref_type = require_safe("core.update.git.reference.name")
 
-if not (close_process and close_timer and commit and ref_type) then
+if not (close_process and close_timer and commit and logger and ref_name and ref_type) then
 	return
 end
 
@@ -34,6 +36,7 @@ local M = {
 local function closehandles()
 	for _, handle in ipairs({
 		M.handle.commit,
+		M.handle.name,
 		M.handle.reference.type,
 	}) do
 		close_process(handle)
@@ -53,9 +56,30 @@ M.run = function()
 		0,
 		500,
 		vim.schedule_wrap(function()
-			commit(M)
-			if M.commit ~= nil then
+			if M.commit == nil then
+				commit(M)
+			end
+			if M.commit ~= nil and M.reference.type == nil then
 				ref_type(M)
+			end
+			if M.reference.type ~= nil and M.reference.name == nil then
+				ref_name(M)
+			end
+			if M.reference.name ~= nil then
+				if M.reference.type == "branch" then
+					if M.reference.name == "main" then
+						logger.info("Download main")
+					end
+					logger.INFO(
+						"TYPE branch detected. Checking if it is main. If it is main, compare dates, if remote newer, than give update notification."
+					)
+				elseif M.reference.type == "ref" then
+					logger.INFO(
+						"TYPE ref detected. CHECK if installed nvim version is equal to ref version. Else check if version exists. Then notify user about that."
+					)
+				else
+					logger.INFO("On stale commit. notify that update is only possible on a stable ref or main.")
+				end
 			end
 		end)
 	)
