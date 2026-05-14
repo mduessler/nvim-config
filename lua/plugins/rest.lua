01b4fe9f8806bf8e75294dccc37d5ba7b1b957eb
Max Düssler <max.duessler@posteo.net>
2026-05-14 21:36:35 +0200

    Fixed rest plugin bugs.

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,10 +1,14 @@
 return {
 	"rest-nvim/rest.nvim",
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			opts.ensure_installed = opts.ensure_installed or {}
-			table.insert(opts.ensure_installed, "http")
-		end,
+		"nvim-neotest/nvim-nio",
+		{
+			"nvim-treesitter/nvim-treesitter",
+			opts = function(_, opts)
+				opts.ensure_installed = opts.ensure_installed or {}
+				table.insert(opts.ensure_installed, "http")
+			end,
+		},
+		{ "j-hui/fidget.nvim", opts = {} },
 	},
 }
