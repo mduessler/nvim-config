ddfb98fee8291233bf8934b93c6e096b56244ec7
Max Düssler <max.duessler@posteo.net>
2026-05-14 20:56:49 +0200

    Set config tabs for json file

--- lua/config/tab.lua
+++ lua/config/tab.lua
@@ -23,3 +23,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.expandtab = true
 	end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "json",
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
+})
