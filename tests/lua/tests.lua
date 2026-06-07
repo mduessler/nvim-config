e35c77177b46733ba06f8a04cdd29580f4d8de02
Max Düssler <max.duessler@posteo.net>
2026-06-07 12:26:59 +0200

    fix: Reset tests.

--- tests/lua/tests.lua
+++ tests/lua/tests.lua
@@ -1,15 +1,3 @@
 local lu = require("luaunit")
 
-vim = {
-	notify = function(_, _) end,
-	log = {
-		levels = {
-			WARN = 1,
-			ERROR = 2,
-		},
-	},
-}
-
-require("tests.lua.utils")
-
 os.exit(lu.LuaUnit.run())
