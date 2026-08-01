75e086f6c5dd399e2ac7476c5b9cfd034a8b737b
Max Düssler <max.duessler@posteo.net>
2026-08-01 21:00:40 +0200

    refactor: downgrade rest.nvim

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,14 +1,14 @@
 return {
 	"rest-nvim/rest.nvim",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		{
-			"nvim-treesitter/nvim-treesitter",
-			opts = function(_, opts)
-				opts.ensure_installed = opts.ensure_installed or {}
-				table.insert(opts.ensure_installed, "http")
-			end,
-		},
-		{ "j-hui/fidget.nvim", opts = {} },
-	},
+	tag = "v2.0.1", -- letzte Version ohne LuaRocks-Abhängigkeiten
+	ft = "http",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("rest-nvim").setup()
+
+		vim.keymap.set("n", "<leader>h", "<NOP>", { desc = "HTTP (rest.nvim)" })
+		vim.keymap.set("n", "<leader>hr", "<Plug>RestNvim", { desc = "Run request under cursor" })
+		vim.keymap.set("n", "<leader>hp", "<Plug>RestNvimPreview", { desc = "Preview cURL command" })
+		vim.keymap.set("n", "<leader>hl", "<Plug>RestNvimLast", { desc = "Re-run last request" })
+	end,
 }
