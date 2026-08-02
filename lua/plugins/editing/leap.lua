cd510fe1d18e73d3ab00b01234dabd753d449fbe
Max Düssler <max.duessler@posteo.net>
2026-08-02 12:34:51 +0200

    fix: move leap to gs/gS to avoid clashing with sandwich

--- lua/plugins/editing/leap.lua
+++ lua/plugins/editing/leap.lua
@@ -1,6 +1,7 @@
 return {
 	url = "https://codeberg.org/andyg/leap.nvim",
 	config = function()
-		require("leap").add_default_mappings()
+		vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-forward)", { desc = "Leap forward" })
+		vim.keymap.set({ "n", "x", "o" }, "gS", "<Plug>(leap-backward)", { desc = "Leap backward" })
 	end,
 }
