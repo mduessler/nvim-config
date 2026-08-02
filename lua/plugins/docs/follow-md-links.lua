22bbf1f7a805ac54ea940c18697a43cd458ea6cc
Max Düssler <max.duessler@posteo.net>
2026-08-02 12:34:58 +0200

    fix: make follow-md-links backspace mapping buffer local

--- lua/plugins/docs/follow-md-links.lua
+++ lua/plugins/docs/follow-md-links.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = { "markdown" },
-			callback = function()
-				vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true })
+			callback = function(event)
+				vim.keymap.set("n", "<bs>", ":edit #<cr>", { silent = true, buffer = event.buf })
 			end,
 		})
 	end,
