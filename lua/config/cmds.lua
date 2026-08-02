6c7afd5c0611f4a5e4d04babcfbcb2c053244597
Max Düssler <max.duessler@posteo.net>
2026-08-02 07:30:30 +0200

    fix: Make InitNVIM report progress live and fail on broken packages

--- lua/config/cmds.lua
+++ lua/config/cmds.lua
@@ -1,58 +1,82 @@
 vim.api.nvim_create_user_command("InitNVIM", function()
-	if #vim.api.nvim_list_uis() == 0 then -- headless
-		vim.cmd("Lazy! sync")
-		vim.wait(5000)
-
-		local formaters = require("lsp.formater")
-		local linters = require("lsp.linter")
-		local registry = require("mason-registry")
-		local servers = require("lsp.servers")
-
-		local function install_packages(package)
-			if not registry.has_package(package) then
-				print("Don't know package " .. package)
-			end
-			local pkg = registry.get_package(package)
-			if pkg:is_installed() then
-				print("Already installed: " .. package)
-				return
-			end
-			print("Installing " .. package .. " ...")
-			local done = false
-			pkg:install():once("closed", function()
-				done = true
-			end)
-			vim.wait(600000, function()
-				return done
-			end, 500)
-			if done then
-				print("Installed: " .. package)
-			else
-				print("Failed to install " .. package)
-			end
-		end
-
-		print("Installing Language Servers.")
-		for package, _ in pairs(servers) do
-			install_packages(package)
-		end
-
-		print("Installing Formater")
-		for package, _ in pairs(formaters) do
-			if package ~= "rustfmt" then
-				install_packages(package)
-			end
-		end
-
-		print("Installing Linter")
-		for package, _ in pairs(linters) do
-			install_packages(package)
-		end
-
-		vim.wait(5000)
-
-		print("Installing Treesitter languages.")
-		require("nvim-treesitter").install({ "all" }):wait(1800000)
-		vim.cmd("qa!")
+	if #vim.api.nvim_list_uis() ~= 0 then
+		return
 	end
+
+	-- Write progress unbuffered so e.g. `docker build` shows it live.
+	local function log(msg)
+		io.write(msg .. "\n")
+		io.flush()
+	end
+
+	vim.cmd("Lazy! sync")
+	vim.wait(5000)
+
+	local formaters = require("lsp.formater")
+	local linters = require("lsp.linter")
+	local registry = require("mason-registry")
+	local servers = require("lsp.servers")
+
+	local failures = 0
+
+	log("Refreshing mason registry ...")
+	registry.refresh()
+
+	local function install_package(package)
+		if not registry.has_package(package) then
+			log("Don't know package " .. package)
+			failures = failures + 1
+			return
+		end
+		local pkg = registry.get_package(package)
+		if pkg:is_installed() then
+			log("Already installed: " .. package)
+			return
+		end
+		log("Installing " .. package .. " ...")
+		local done = false
+		pkg:install():once("closed", function()
+			done = true
+		end)
+		vim.wait(600000, function()
+			return done
+		end, 500)
+		-- "closed" also fires for failed installations, so verify the result.
+		if done and pkg:is_installed() then
+			log("Installed: " .. package)
+		else
+			log("Failed to install " .. package)
+			failures = failures + 1
+		end
+	end
+
+	log("Installing Language Servers.")
+	for package, _ in pairs(servers) do
+		install_package(package)
+	end
+
+	log("Installing Formater")
+	for package, _ in pairs(formaters) do
+		if package ~= "rustfmt" then
+			install_package(package)
+		end
+	end
+
+	log("Installing Linter")
+	for package, _ in pairs(linters) do
+		install_package(package)
+	end
+
+	vim.wait(5000)
+
+	log("Installing Treesitter languages.")
+	require("nvim-treesitter").install({ "all" }):wait(1800000)
+
+	if failures > 0 then
+		log("InitNVIM finished with " .. failures .. " failed package installation(s).")
+		vim.cmd("cquit! 1")
+	end
+
+	log("InitNVIM finished successfully.")
+	vim.cmd("qa!")
 end, { desc = "Initalize plugins, lsps and Treesitter" })
